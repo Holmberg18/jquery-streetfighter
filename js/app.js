626be8dd1302d53523146ba6e07e511957a8502c
Jon Holmberg <holmberg18@gmail.com>
2016-07-08 18:38:21 -0700

    2.4 complete

--- js/app.js
+++ js/app.js
@@ -8,6 +8,37 @@ $(document).ready(function(){
 			// alert('mouse left');
 			$('.ryu-ready').hide();
 			$('.ryu-still').show();
+			$('.ryu-throwing').hide();
+			$('.hadouken').hide();
+	})
+	
+		.mousedown(function() {
+			playHadouken();
+			$('.ryu-ready').hide();
+			$('.ryu-throwing').show();
+			$('.hadouken').finish().show()
+			.animate(
+			{'left':'1020px'},
+			500,
+			function() {
+				$(this).hide();
+				$(this).css('left','520px');
+			   }
+			);
+		})
 		
-	});
+		.mouseup(function() {
+			console.log('mouseup');
+			$('.ryu-ready').show();
+			$('.ryu-throwing').hide();
+			// $('.hadouken').hide();
+			
+			//ryu goes back to his ready position
+		});
 });
+
+function playHadouken () {
+		$('#hadouken-sound')[0].volume = 0.5;
+		$('#hadouken-sound')[0].load();
+		$('#hadouken-sound')[0].play();
+	}
